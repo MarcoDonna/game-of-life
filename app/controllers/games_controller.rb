e5b24042839c6656d8e4cc26d31f98973370383b
Marco Donna <marco.donna@gmail.com>
2025-09-26 21:03:39 +0200

    add new and create to GamesController

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -4,4 +4,28 @@ class GamesController < ApplicationController
   def index
     @games = current_user.games
   end
+
+  def new
+    @game = Game.new(user: current_user)
+  end
+
+  def create
+    @game = Game.new(create_game_params)
+    @game.user = current_user
+
+    if(@game.save)
+      board_size = @game.board_width * @game.board_height
+      initial_frame = GameFrame.create(game: @game, state: Array.new(board_size) { ["*", "."].sample }.join())
+
+      redirect_to games_url
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def create_game_params
+    params.expect(game: [:board_width, :board_height])
+  end
 end
--- app/helpers/games_helper.rb
+++ app/helpers/games_helper.rb
@@ -1,5 +1,6 @@
 module GamesHelper
     def last_frame_preview(game)
-        content_tag(:p, safe_join(game.frames.last.state.scan(Regexp.new(".{1,#{game.board_width}}")), tag(:br)))
+        last_frame = game.frames.last
+        content_tag(:p, safe_join(last_frame.state.scan(Regexp.new(".{1,#{game.board_width}}")), tag(:br))) unless last_frame&.state.nil?
     end
 end
