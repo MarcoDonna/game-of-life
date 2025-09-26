ab5bc541ed78e958b90cc04795235553cd6b9a89
Marco Donna <marco.donna@gmail.com>
2025-09-26 18:32:55 +0200

    generate game model and run migrations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_09_26_162343) do
+ActiveRecord::Schema[8.0].define(version: 2025_09_26_163016) do
+  create_table "games", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.integer "board_width"
+    t.integer "board_height"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_games_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -22,4 +31,6 @@ ActiveRecord::Schema[8.0].define(version: 2025_09_26_162343) do
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
+
+  add_foreign_key "games", "users"
 end
