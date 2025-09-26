8ed179b8f25efac604a4e0f35219d38ea9d388ef
Marco Donna <marco.donna@gmail.com>
2025-09-26 21:25:57 +0200

    add template for game_frames->show

--- app/controllers/game_frames_controller.rb
+++ app/controllers/game_frames_controller.rb
@@ -1,4 +1,16 @@
 class GameFramesController < ApplicationController
+  before_action :authenticate_user!
+  before_action :find_frame
+
   def show
+    @prev_frame = @frame.prev_frame
+  end
+
+  private
+
+  def find_frame
+    @frame = GameFrame.find(params[:id])
+
+    redirect_to games_url unless @frame.game.user = current_user
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  get "game_frames/show"
   devise_for :users
   
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
@@ -7,6 +6,8 @@ Rails.application.routes.draw do
 
   get "games/new", to: "games#new", as: :new_game
   post "games", to: "games#create", as: :create_game
+  
+  get "game_frames/:id/show", to: "game_frames#show", as: :show_game_frame
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
